5aaa0815cbfab170da4d01f8ac9d69983e99b71a
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-05-26 12:15:31 +0200

    Update index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,19 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_route() -> str:
+    """ GET /api/v1/unauthorized
+    Raises 401 error
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden_route() -> str:
+    """ GET /api/v1/forbidden
+    Raises 403 error
+    """
+    abort(403)
