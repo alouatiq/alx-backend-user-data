3955b048d7fa2da35214515b5d913f6b77b178ac
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-06-05 14:41:48 +0200

    Update main.py

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -62,7 +62,9 @@ def update_password(email: str, reset_token: str, new_password: str) -> None:
         "reset_token": reset_token,
         "new_password": new_password
     }
-    response = requests.put(f"{BASE_URL}/reset_password", data=data)
+    response = requests.put(
+        f"{BASE_URL}/reset_password", data=data
+    )
     assert response.status_code == 200
     assert response.json() == {"email": email, "message": "Password updated"}
 
