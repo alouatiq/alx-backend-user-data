19c0b9fd47b7c9097262885bb05cd2e3e2bb6666
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-06-12 21:14:12 +0200

    Update user.py

--- 0x02-Session_authentication/models/user.py
+++ 0x02-Session_authentication/models/user.py
@@ -1,59 +1,69 @@
 #!/usr/bin/env python3
-""" User module
-"""
-import hashlib
-from models.base import Base
+"""User model."""
+import uuid
+from datetime import datetime
+from models import storage
+from hashlib import md5
 
 
-class User(Base):
-    """ User class
-    """
+class User:
+    """A minimal User model storing email & hashed password."""
 
-    def __init__(self, *args: list, **kwargs: dict):
-        """ Initialize a User instance
-        """
-        super().__init__(*args, **kwargs)
-        self.email = kwargs.get('email')
-        self._password = kwargs.get('_password')
-        self.first_name = kwargs.get('first_name')
-        self.last_name = kwargs.get('last_name')
+    def __init__(self, *args, **kwargs):
+        """Create new or load from kwargs dict."""
+        if kwargs:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
 
+    # ---------------- Password helpers ---------------- #
     @property
-    def password(self) -> str:
-        """ Getter of the password
-        """
+    def password(self):
+        """Password getter (hash is stored)."""
         return self._password
 
     @password.setter
-    def password(self, pwd: str):
-        """ Setter of a new password: encrypt in SHA256
-        """
-        if pwd is None or type(pwd) is not str:
-            self._password = None
-        else:
-            self._password = hashlib.sha256(pwd.encode()).hexdigest().lower()
+    def password(self, pwd):
+        """Hash password on set."""
+        self._password = md5(pwd.encode()).hexdigest()
 
     def is_valid_password(self, pwd: str) -> bool:
-        """ Validate a password
-        """
-        if pwd is None or type(pwd) is not str:
-            return False
-        if self.password is None:
-            return False
-        pwd_e = pwd.encode()
-        return hashlib.sha256(pwd_e).hexdigest().lower() == self.password
+        """Check plaintext password against stored hash."""
+        return self._password == md5(pwd.encode()).hexdigest()
 
-    def display_name(self) -> str:
-        """ Display User name based on email/first_name/last_name
-        """
-        if self.email is None and self.first_name is None \
-                and self.last_name is None:
-            return ""
-        if self.first_name is None and self.last_name is None:
-            return "{}".format(self.email)
-        if self.last_name is None:
-            return "{}".format(self.first_name)
-        if self.first_name is None:
-            return "{}".format(self.last_name)
-        else:
-            return "{} {}".format(self.first_name, self.last_name)
+    # ---------------- Storage helpers ----------------- #
+    def save(self):
+        """Save/update object to storage."""
+        self.updated_at = datetime.utcnow()
+        storage.new(self)
+        storage.save()
+
+    def to_json(self, save_to_file=False):
+        """Return dict representation for JSON serialization."""
+        d = self.__dict__.copy()
+        d["__class__"] = self.__class__.__name__
+        # hide hashed pwd when sending to client
+        if not save_to_file:
+            d.pop("_password", None)
+        return d
+
+    # ---------------- Class helpers ------------------- #
+    @classmethod
+    def get(cls, user_id: str):
+        """Retrieve a user by ID."""
+        objs = storage.all(cls)
+        key = "{}.{}".format(cls.__name__, user_id)
+        return objs.get(key)
+
+    @classmethod
+    def search(cls, attrs: dict):
+        """Naive search by attributes (supports only email for this project)."""
+        result = []
+        for obj in storage.all(cls).values():
+            match = all(getattr(obj, k) == v for k, v in attrs.items())
+            if match:
+                result.append(obj)
+        return result
